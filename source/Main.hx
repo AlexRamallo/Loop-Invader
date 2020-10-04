df33e485099796fa1d089a5fb8284dc2fa0bb199
Alejandro Ramallo <alejandroramallo@gmail.com>
2020-10-04 01:17:46 -0400

    audiofix

--- source/Main.hx
+++ source/Main.hx
@@ -2,6 +2,7 @@ package;
 
 import flixel.FlxGame;
 import openfl.display.Sprite;
+import openfl.Assets;
 import flixel.FlxG;
 
 class Main extends Sprite
@@ -14,6 +15,13 @@ class Main extends Sprite
 		FlxG.sound.volumeDownKeys = [];
 		FlxG.sound.volumeUpKeys = [];
 
-		FlxG.sound.playMusic(AssetPaths.bgm__ogg, 0.7, true);
+		Assets.getSound('assets/sounds/bgm.ogg').play(0.0, 1000000);
+
+		#if js
+		untyped {
+			document.oncontextmenu = document.body.oncontextmenu = function() {return false;}
+		}
+		#end
+
 	}
 }
